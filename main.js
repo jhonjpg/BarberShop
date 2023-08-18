b579ca3bb05e983a89970a3fba2f60512c16c661
Jonathan  Peña <102936768+jhonjpg@users.noreply.github.com>
2023-08-18 11:23:44 -0400

    BARBERSHOP

--- main.js
+++ main.js
@@ -64,3 +64,51 @@ masInformacionBtn.addEventListener('click', () => {
         textoAdicional.classList.add('visible');
     }
 });
+
+
+const ball = document.querySelector('.ball');
+const ballin = document.querySelector('.arrow');
+
+
+window.addEventListener('scroll', () => {
+    const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolled = window.scrollY;
+
+    const height = Math.min(100, (scrolled / scrollHeight) * 80);
+    ball.style.height = `${height}px`;
+
+    if (height >= 80) {
+        ball.style.backgroundColor = '#fbcc15';
+    } else {
+        ball.style.backgroundColor = '#fbcc15';
+    }
+
+
+    if (height >= 80) {
+        ballin.style.backgroundColor = '#fbcc15';
+    } else {
+        ballin.style.backgroundColor = 'black';
+    }
+
+
+
+});
+
+
+
+const scrollToTopBtn = document.getElementById('scrollToTop');
+const ballContainer = document.querySelector('.ball-container');
+
+// Mostrar el contenedor de la bola después de recorrer 300px
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 300) {
+        ballContainer.style.opacity = 1;
+    } else {
+        ballContainer.style.opacity = 0;
+    }
+});
+
+// Hacer scroll hacia arriba cuando se hace clic en la flecha
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
